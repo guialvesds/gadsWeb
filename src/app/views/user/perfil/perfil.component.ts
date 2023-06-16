a914e159b7d7ef31891adad6be5de3aff13fbf53
guialvesds <guilherme_ealves@outlook.com>
2023-06-16 20:28:07 -0300

    m

--- src/app/views/user/perfil/perfil.component.ts
+++ src/app/views/user/perfil/perfil.component.ts
@@ -1,7 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { map } from 'rxjs';
+import { map, retry } from 'rxjs';
 import { User } from 'src/app/models/User.model';
 import { USerService } from 'src/app/services/user.service';
 
@@ -12,6 +19,11 @@ import { USerService } from 'src/app/services/user.service';
 })
 export class PerfilComponent implements OnInit {
   public userData!: User | any;
+  public imageUrl: string = '';
+  predefinedImageUrl = '../../../../assets/perfil-image.jpg';
+
+  selectedFile: File | null = null;
+  previewUrl: string | ArrayBuffer | null = null;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: number,
@@ -25,6 +37,30 @@ export class PerfilComponent implements OnInit {
 
   public modifyDate(row: string): string {
     return row.toLocaleString().substring(0, 10).split('-').reverse().join('/');
+  }
+
+  public onFileSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.previewUrl = reader.result as string;
+      };
+      reader.readAsDataURL(fileInput.files[0]);
+    } else {
+      this.previewUrl = null;
+    }
+  }
+
+  public unsetImage(): string {
+    return (this.previewUrl = '');
+  }
+
+  public upload(): void {
+    // Aqui você pode adicionar a lógica para enviar a foto para a sua API
+    // usando uma biblioteca como o HttpClient do Angular
+    // Exemplo: this.http.post('sua-api.com/upload', this.selectedFile);
+    console.log('Foto enviada:', this.selectedFile);
   }
 
   private findUser(): void {
--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -4,14 +4,25 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { PerfilComponent } from './perfil/perfil.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    PerfilComponent
-  ],
+  declarations: [PerfilComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
-  ]
+    UserRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
 })
-export class UserModule { }
+export class UserModule {}
